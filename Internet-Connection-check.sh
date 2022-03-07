f0498ce2a38bb029dbf09d8a5c57893d0714ac93
Shougo MATSUDA <jamming6123@gmail.com>
2022-03-08 07:24:49 +0900

    First Commit

---
+++ Internet-Connection-check.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+while true
+do
+	while read line
+	do
+		echo "====="
+		sleep 1
+		echo $line
+		ping -c1 $line
+		if [ $? -ne 0 ]; then
+			DATE=`date '+%Y/%m/%d %H:%M:%S'`
+			osascript -e "display notification \"Internet Connection途絶(IPv4)の可能性高。注意確認の事！\" with title \"$DATE\""
+		fi
+	done < ./IPv4-list.txt
+
+	sleep 2
+
+	while read line
+	do
+		echo "====="
+		sleep 1
+    	echo $line
+    	ping6 -c1 $line
+    	if [ $? -ne 0 ]; then
+			DATE=`date '+%Y/%m/%d %H:%M:%S'`
+			osascript -e "display notification \"Internet Connection途絶(IPv6)の可能性高。注意確認の事！\" with title \"$DATE\""
+		fi
+	done < ./IPv6-list.txt
+
+	sleep 2
+
+done
+
